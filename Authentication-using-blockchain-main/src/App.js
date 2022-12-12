d62aac2f7da3130a19cdd976937cda6af64f993b
Parag Lawate <lawatepv@rknec.edu>
2022-12-12 19:30:26 +0530

    mini project uploading

---
+++ Authentication-using-blockchain-main/src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import SignIn from "./Screens/Signin";
+import SignUp from "./Screens/Signup";
+import Home from "./Screens/Home";
+
+function App() {
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<SignIn />} />
+          <Route path="/Signup" element={<SignUp />} />
+          <Route
+            path="/Home"
+            element={<Home />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
